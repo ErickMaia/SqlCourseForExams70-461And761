ca29d16b42579b599d72e1c685c52c7381245e39
Erick Maia <erickmaiatmariano@gmail.com>
2023-04-13 12:45:53 -0300

    23. Deleting the data, then the table

---
+++ 23. Deleting the data, then the table.sql
@@ -0,0 +1,19 @@
+-- FIRST TABLE
+
+SELECT * FROM FirstTable
+
+DELETE FROM FirstTable
+
+SELECT * FROM FirstTable
+
+DROP TABLE FirstTable
+
+-- SECOND TABLE
+
+SELECT * FROM SecondTable
+
+TRUNCATE TABLE SecondTable
+
+SELECT * FROM SecondTable
+
+DROP TABLE SecondTable
